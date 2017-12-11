be792c422f64b0a5a9baaafa56753fb527da805b
Bugscript <34430540+Bugscript@users.noreply.github.com>
2017-12-11 08:22:29 -0600

    Update and rename index.js to server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+//server.js file
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
